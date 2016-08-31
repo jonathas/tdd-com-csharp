764985e01be86d70d4cb500b1103e10999c7f0be
Jon Ribeiro <contact@jonathas.com>
2016-08-31 23:14:36 +0200

    Implemented first test

--- Caelum.Leilao/TesteDoAvaliador.cs
+++ Caelum.Leilao/TesteDoAvaliador.cs
@@ -1,28 +1,35 @@
 ﻿using System;
+using NUnit.Framework;
 
 namespace Caelum.Leilao
 {
+	[TestFixture]
 	public class TesteDoAvaliador
 	{
-		static void Main(String[] args) 
+		[Test]
+		public void Main() 
 		{
+			// 1ª parte: cenario
 			Usuario joao = new Usuario ("Joao");
 			Usuario jose = new Usuario ("Jose");
 			Usuario maria = new Usuario ("Maria");
 
 			Leilao leilao = new Leilao ("Playstation 3 Novo");
 
+			leilao.Propoe (new Lance (maria, 250.0));
 			leilao.Propoe (new Lance (joao, 300.0));
 			leilao.Propoe (new Lance (jose, 400.0));
-			leilao.Propoe (new Lance (maria, 250.0));
 
+			// 2ª parte: acao
 			Avaliador leiloeiro = new Avaliador ();
 			leiloeiro.Avalia (leilao);
 
-			Console.WriteLine (leiloeiro.MaiorLance);
-			Console.WriteLine (leiloeiro.MenorLance);
+			// 3ª parte: validacao
+			double maiorEsperado = 400;
+			double menorEsperado = 250;
 
-			Console.ReadKey ();
+			Assert.AreEqual(maiorEsperado, leiloeiro.MaiorLance);
+			Assert.AreEqual(menorEsperado, leiloeiro.MenorLance);
 		}
 	}
 }
