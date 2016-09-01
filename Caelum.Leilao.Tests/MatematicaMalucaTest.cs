ef8b2f03ec02d28e0bef27382a695e868e6999a9
Jon Ribeiro <jonathas.ribeiro@mautilus.com>
2016-09-01 18:19:11 +0200

    Added tests for MateMaticaMaluca class

---
+++ Caelum.Leilao.Tests/MatematicaMalucaTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace Caelum.Leilao
+{
+    [TestFixture]
+    public class MatematicaMalucaTest
+    {
+
+        [Test]
+        public void DeveMultiplicarNumerosMaioresQue30()
+        {
+            MatematicaMaluca matematica = new MatematicaMaluca();
+            Assert.AreEqual(50 * 4, matematica.ContaMaluca(50));
+        }
+
+        [Test]
+        public void DeveMultiplicarNumerosMaioresQue10EMenoresQue30()
+        {
+            MatematicaMaluca matematica = new MatematicaMaluca();
+            Assert.AreEqual(20 * 3, matematica.ContaMaluca(20));
+        }
+
+        [Test]
+        public void DeveMultiplicarNumerosMenoresQue10()
+        {
+            MatematicaMaluca matematica = new MatematicaMaluca();
+            Assert.AreEqual(5 * 2, matematica.ContaMaluca(5));
+        }
+    }
+}
---
+++ Caelum.Leilao/MatematicaMaluca.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Caelum.Leilao
+{
+    public class MatematicaMaluca
+    {
+        public int ContaMaluca(int numero)
+        {
+            if (numero > 30) return numero * 4;
+            else if (numero > 10) return numero * 3;
+            else return numero * 2;
+        }
+    }
+}
