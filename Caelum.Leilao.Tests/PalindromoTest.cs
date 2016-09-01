8092d7ae62607b9cc6e2db39d3eb3f13f0db7953
Jon Ribeiro <jonathas.ribeiro@mautilus.com>
2016-09-01 15:02:33 +0200

    Tested a palindrome

---
+++ Caelum.Leilao.Tests/PalindromoTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Caelum.Leilao;
+using NUnit.Framework;
+
+namespace Caelum.Leilao
+{
+    [TestFixture]
+    class PalindromoTest
+    {
+        [Test]
+        public void DeveIdentificarPalindromo()
+        {
+            Palindromo palindromo = new Palindromo();
+            bool res = palindromo.EhPalindromo("Socorram-me subi no onibus em Marrocos");
+            Assert.IsTrue(res);
+        }
+
+        [Test]
+        public void DeveIdentificarSeNaoEhPalindromo()
+        {
+            Palindromo palindromo = new Palindromo();
+            bool res = palindromo.EhPalindromo("E preciso amar as pessoas como se nao houvesse amanha");
+            Assert.IsFalse(res);
+        }
+    }
+}
---
+++ Caelum.Leilao/Palindromo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Caelum.Leilao
+{
+    public class Palindromo
+    {
+        public bool EhPalindromo(String frase)
+        {
+            String fraseFiltrada = frase.ToUpper().Replace(" ", "").Replace("-", "");
+
+            for (int i = 0; i < fraseFiltrada.Length; i++)
+            {
+                if (fraseFiltrada[i] != fraseFiltrada[fraseFiltrada.Length - i - 1])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
